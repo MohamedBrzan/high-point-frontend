73093bd5a8185cf02e032ad64a3b1e030ac64bb4
mohamed mahmoud <mohamedbrzan11@gmail.com>
2022-11-16 07:32:35 +0200

    Deploying App

--- src/pages/Authentication/Login.js
+++ src/pages/Authentication/Login.js
@@ -112,9 +112,9 @@ const LoginPage = () => {
               </Button>
               <p className='text-center my-3'>{t('or_text')}</p>
 
-              <GoogleLoginButton
+              {/* <GoogleLoginButton
               // googleLogin={googleLogin}
-              />
+              /> */}
 
               <Row>
                 {' '}
